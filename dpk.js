f4badb41a43ee0e2b422da8539b3f197636dddfa
Gabriel Rios <gabrielfrios@gmail.com>
2023-02-28 22:06:42 -0300

    refactor deterministicPartitionKey

--- dpk.js
+++ dpk.js
@@ -1,28 +1,26 @@
 const crypto = require("crypto");
 
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
+
+const stringify = (value) => {
+  return typeof value !== "string" ? JSON.stringify(value) : value
+}
+
+const createHash = (value) => {
+  return crypto.createHash("sha3-512").update(stringify(value)).digest("hex")
+}
+
+const isNullOrUndefined = (value) => {
+  return typeof value === "undefined" || value === null
+}
+
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
-
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (isNullOrUndefined(event)) {
+    return TRIVIAL_PARTITION_KEY;
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+  const candidate = event.partitionKey ? stringify(event.partitionKey) : createHash(event)
+
+  return candidate.length > MAX_PARTITION_KEY_LENGTH ? createHash(candidate) : candidate
 };
